703215f595013b4c25f571b05fdad0dbbe6f3e5e
donishabduvaliyev <donishkratos413677@gmail.com>
2025-09-03 02:13:03 +0500

    sg

--- models/Order.js
+++ models/Order.js
@@ -3,17 +3,46 @@ import mongoose from 'mongoose';
 const orderSchema = new mongoose.Schema({
     shop: { type: mongoose.Schema.Types.ObjectId, ref: 'Shop', required: true },
     customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
     items: [{
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        name: { type: String, required: true },  
+        price: { type: Number, required: true }, 
         quantity: { type: Number, required: true, min: 1 }
     }],
+
     total: { type: Number, required: true, min: 0 },
-    status: { type: String, enum: ['pending', 'confirmed', 'completed', 'cancelled'], default: 'pending' },
+
+    status: { 
+        type: String, 
+        enum: ['pending', 'confirmed', 'completed', 'cancelled'], 
+        default: 'pending' 
+    },
+
+
     customerName: { type: String, required: true },
     customerPhone: { type: String, required: true },
     customerAddress: { type: String, required: true },
     customerTelegramId: { type: String },
-    createdAt: { type: Date, default: Date.now }
-});
+
+  
+    paymentStatus: { 
+        type: String, 
+        enum: ['unpaid', 'paid'], 
+        default: 'unpaid' 
+    },
+    deliveryMethod: { 
+        type: String, 
+        enum: ['pickup', 'delivery'], 
+        default: 'delivery' 
+    },
+    deliveryFee: { type: Number, default: 0 },
+
+    notes: { type: String },
+    isDeleted: { type: Boolean, default: false }
+
+}, { timestamps: true });
+
+orderSchema.index({ shopId: 1, status: 1, createdAt: -1  ,customer: 1,});
 
 export default mongoose.model('Order', orderSchema);
--- models/Product.js
+++ models/Product.js
@@ -3,13 +3,16 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     category: { type: String, required: true },
-    
+    variant: { type: String, required: true }, 
     price: { type: Number, required: true },
     description: { type: String },
-    image: { type: String }, // URL to product image
+    image: { type: String }, 
     shop: { type: mongoose.Schema.Types.ObjectId, ref: 'Shop', required: true },
     isActive: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now }
 });
+
+
+productSchema.index({ name: "text", description: "text" , shop: 1 , createdAt: -1 });
 
 export default mongoose.model('Product', productSchema );
